50e3d176971947d3ad6510ee52b8ed07fe43a502
Jack Long <putheara.long@gmail.com>
2020-04-08 00:43:50 -0400

    testing readme generation

---
+++ Generated_README.md
@@ -0,0 +1,39 @@
+
+      
+    # new
+
+    ### Table of Content
+        * [Description]
+        * [Usage]
+        * [Installation]
+        * [License]
+        * [Contributing]
+        * [Test]
+        * [Questions]
+
+    ### Description
+       ok
+
+    ###Usage
+        install
+    
+    ###Installation
+        hit enter
+    
+    ###License
+        mit
+
+    ###Contributing
+        undefined
+    
+    ###Test
+        npm test
+
+    ###Questions
+        undefined
+
+
+    Format: ![github avatar] (https://avatars2.githubusercontent.com/u/60330956?v=4)     
+      
+      
+      
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquire = require("inquirer");
 const fs = require("fs");
 const axios = require("axios");
-const MarkDownit = require("markdown-it");
+
 
 
 
@@ -22,6 +22,42 @@ const questions = [{
     type: "input",
     name: "inputDescription",
     message: "Please enter the description of your project."
+},
+
+{
+    type: "input",
+    name: "inputUsage",
+    message: "Please enter the usage of your project."
+},
+
+{
+    type: "input",
+    name: "inputInstallation",
+    message: "Please enter how to install your project."
+},
+
+{
+    type: "input",
+    name: "inputLicense",
+    message: "Please select the license for your project."
+},
+
+{
+    type: "input",
+    name: "inputContributing",
+    message: "Please enter the names of people contributing to the project."
+},
+
+{
+    type: "input",
+    name: "inputTest",
+    message: "Please enter the description of how to test your project."
+},
+
+{
+    type: "input",
+    name: "inputQuestion",
+    message: "Please ask question about the project."
 }
 
 ];
@@ -33,32 +69,69 @@ function userInfoPrompt() {
 var username;
 var title;
 var description;
+var usage;
+var license;
+var contributing;
+var test;
+var question;
 
 userInfoPrompt().then(function(answer){
-      username = answer.userInput;
-      title = answer.inputTitle;
-      description = answer.inputDescription;
+    username = answer.userInput;
+    title = answer.inputTitle;
+    description = answer.inputDescription;
+    usage = answer.inputUsage;
+    installation = answer.inputInstallation;
+    license = answer.inputLicense;
+    test = answer.inputTest;
+    question = answer.inputQuestin;
 
-      const queryUrl = `https://api.github.com/users/${username}/repos?per_page=25`;
+    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=25`;
 
-      axios.get(queryUrl).then(function(returnedObj){
-      var repoOwner =  returnedObj.data[0].owner;
-      console.log(repoOwner.avatar_url);
+    axios.get(queryUrl).then(function(returnedObj){
+    var repoOwner =  returnedObj.data[0].owner;
+    console.log(repoOwner.avatar_url);
 
-      let markDown = `
+    let markDown = `
       
-      # ${title}
+    # ${title}
 
-      # ${description}
+    ### Table of Content
+        * [Description]
+        * [Usage]
+        * [Installation]
+        * [License]
+        * [Contributing]
+        * [Test]
+        * [Questions]
 
-      ![github avatar] (${repoOwner.avatar_url})     
+    ### Description
+       ${description}
+
+    ###Usage
+        ${usage}
+    
+    ###Installation
+        ${installation}
+    
+    ###License
+        ${license}
+
+    ###Contributing
+        ${contributing}
+    
+    ###Test
+        ${test}
+
+    ###Questions
+        ${question}
+
+
+    Format: ![github avatar] (${repoOwner.avatar_url})     
       
       
       `
-    md = new MarkDownit();
-    var newMD = md.render(markDown);
-      
-    fs.writeFile("README.md", newMD, function(error){
+         
+    fs.writeFile("Generated_README.md", markDown, function(error){
             if (error) throw error;
             console.log('The file has been saved!');      });
 });
