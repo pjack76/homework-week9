1de4ba5f33f3334b1adfbac6fd1b83c9bcbd0f64
Jack Long <putheara.long@gmail.com>
2020-04-07 22:46:27 -0400

    got more things to work

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# homework-week9
+
+      
+      # new proj
+
+      # great project
+
+      ![github avatar] (https://avatars2.githubusercontent.com/u/60330956?v=4)     
+      
+      
+      
--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const inquire = require("inquirer");
-const newAPI = require("./utils/api")
+const fs = require("fs");
+const axios = require("axios");
+
 
 
 
 const questions = [{
     type: "input",
-    name: "username",
+    name: "userInput",
     message: "Please enter your gitHub username."
 },
 
 {
     type: "input",
-    name: "title",
+    name: "inputTitle",
     message: "What is the title of your project?."
 },
 
 {
     type: "input",
-    name: "description",
+    name: "inputDescription",
     message: "Please enter the description of your project."
 }
 
@@ -27,17 +29,64 @@ function userInfoPrompt() {
     return inquire.prompt(questions)
 };
 
-newAPI.getUser().then(userInfoPrompt());
+var username;
+var title;
+var description;
+
+userInfoPrompt().then(function(answer){
+      username = answer.userInput;
+      title = answer.inputTitle;
+      description = answer.inputDescription;
+
+      const queryUrl = `https://api.github.com/users/${username}/repos?per_page=25`;
+
+      axios.get(queryUrl).then(function(returnedObj){
+      var repoOwner =  returnedObj.data[0].owner;
+      console.log(repoOwner.avatar_url);
+
+      let markDown = `
+      
+      # ${title}
+
+      # ${description}
+
+      ![github avatar] (${repoOwner.avatar_url})     
+      
+      
+      `
+
+      
+      fs.writeFile("README.md", markDown, function(error){
+            if (error) throw error;
+            console.log('The file has been saved!');      });
+});
+});
+
+/*function generateMarkdown() {
+    return `
+  # ${title}
+
+  # ${description}
+  
+  `;
+  }
+
+
+
+
 
 
 
 
 function writeToFile(fileName, data) {
-}
+    fs.writeFile("README.md", generateMarkdown());
+};
+
+writeToFile();
 
 function init() {
 
 }
 
 init();
-
+*/
